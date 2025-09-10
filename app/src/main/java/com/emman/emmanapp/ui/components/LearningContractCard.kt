2656e0a9ab3c0dd9f06fc2d111c6f6f9fa886ddd
Juan Miguel Recondo <juanmiguel.recondo@gmail.com>
2025-09-10 20:09:55 +0800

    feat: bold the card title

--- app/src/main/java/com/emman/emmanapp/ui/components/LearningContractCard.kt
+++ app/src/main/java/com/emman/emmanapp/ui/components/LearningContractCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -38,7 +39,8 @@ fun LearningContractCard(
                 textAlign = TextAlign.Center,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 10.dp)
+                    .padding(vertical = 10.dp),
+                fontWeight = FontWeight.Bold
             )
 
             bulletPoints.forEachIndexed { index, point ->
