303b8b360e6b080b5ca31e7283086cccf064a277
Juan Miguel Recondo <juanmiguel.recondo@gmail.com>
2025-09-10 20:03:56 +0800

    feat: add topBar for Scaffold

--- app/src/main/java/com/emman/emmanapp/MainActivity.kt
+++ app/src/main/java/com/emman/emmanapp/MainActivity.kt
@@ -12,7 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
@@ -22,6 +26,7 @@ import com.emman.emmanapp.ui.components.LearningContractCard
 import com.emman.emmanapp.ui.theme.EmmanAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,7 +40,13 @@ class MainActivity : ComponentActivity() {
                     stringResource(R.string.members_title) to stringArrayResource(R.array.members).toList()
                 )
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text("Learning Contract") }
+                        )
+                    }
                     Column(
                         modifier = Modifier
                             .padding(innerPadding)
